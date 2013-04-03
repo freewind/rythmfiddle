b909cce990a7bf05265db4346ccd7254a17480b6
Freewind <nowind_lee@qq.com>
2013-04-03 18:25:08 +0800

    fix #35

--- angular-seed/app/js/filters.js
+++ angular-seed/app/js/filters.js
@@ -9,9 +9,16 @@ angular.module('myApp.filters', []).
         }
     }]).
     filter('codePretty', function () {
+        function escape(str) {
+            if (str) {
+                return str.replace(/[&]/g, "&amp;").replace(/[<]/g, "&lt;").replace(/[>]/g, "&gt;");
+            }
+            return str;
+        }
+
         return function (code, showLineNums) {
             // hack around incorrect tokenization
-            var newV = code.replace('.done-true', 'doneTrue');
+            var newV = escape(code).replace('.done-true', 'doneTrue');
             newV = prettyPrintOne(newV, undefined, showLineNums);
             // hack around incorrect tokenization
             newV = newV.replace('doneTrue', '.done-true');
